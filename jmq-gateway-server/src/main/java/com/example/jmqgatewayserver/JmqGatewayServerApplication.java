c8a10d270fc952160461f3cb18d4cff1da82ad56
JoseManuel159 <josexx741xx@outlook.es>
2025-04-22 20:49:30 -0500

    Descripción del cambio

---
+++ jmq-gateway-server/src/main/java/com/example/jmqgatewayserver/JmqGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmqgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmqGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmqGatewayServerApplication.class, args);
+    }
+
+}
---
+++ jmq-gateway-server/src/test/java/com/example/jmqgatewayserver/JmqGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmqgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmqGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
