230aa373f6b99cf09219adfc9f036c0e63e2153b
JoseManuel159 <josexx741xx@outlook.es>
2025-04-22 20:31:39 -0500

    Descripción del cambio

---
+++ jmq-registry-server/src/test/java/com/example/jmqregistryserver/JmqRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmqregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmqRegistryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
